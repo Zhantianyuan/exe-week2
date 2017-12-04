12e8a673a5d854df5a73335b5c95785540071d84
Zhantianyuan <eeytz3@nottingham.ac.uk>
2017-12-04 13:27:02 +0000

    '12.41'

---
+++ calc_cmake/mingw_dynamic/CMakeFiles/calc.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/zty/nottingham/normal/year2/H62PEP/exe-week2/calc_cmake/calc.cpp" "D:/zty/nottingham/normal/year2/H62PEP/exe-week2/calc_cmake/mingw_dynamic/CMakeFiles/calc.dir/calc.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "D:/zty/nottingham/normal/year2/H62PEP/exe-week2/calc_cmake/mingw_dynamic/CMakeFiles/maths.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
